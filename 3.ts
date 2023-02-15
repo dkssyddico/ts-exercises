b67d05853f2086c454e2ae456421f6d277ec7a6f
dkssyddico <dkssyddico@naver.com>
2023-02-15 11:00:28 +0900

    3rd question

---
+++ 3.ts
@@ -0,0 +1,52 @@
+interface User {
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+interface Admin {
+  name: string;
+  age: number;
+  role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+  },
+  {
+    name: 'Jane Doe',
+    age: 32,
+    role: 'Administrator',
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+  },
+  {
+    name: 'Bruce Willis',
+    age: 64,
+    role: 'World saver',
+  },
+];
+
+export function logPerson(person: Person) {
+  let additionalInformation: string;
+  if ('role' in person) {
+    additionalInformation = person.role;
+  } else {
+    additionalInformation = person.occupation;
+  }
+  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+persons.forEach(logPerson);
+
+/**
+ * reference: https://www.typescriptlang.org/docs/handbook/2/narrowing.html#the-in-operator-narrowing
+ */
