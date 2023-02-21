2debb666102cec623f60e0b09c16d14335d27520
dkssyddico <dkssyddico@naver.com>
2023-02-21 14:22:52 +0900

    10th solution

--- 10.ts
+++ 10.ts
@@ -76,8 +76,38 @@ export type ApiResponse<T> =
       error: string;
     };
 
-export function promisify(arg: unknown): unknown {
-  return null;
+type CallbackBasedAsyncFunction<T> = (
+  callback: (response: ApiResponse<T>) => void
+) => void;
+
+type PromiseBasedAsyncFunction<T> = () => Promise<T>;
+
+export function promisify<T>(
+  fn: CallbackBasedAsyncFunction<T>
+): PromiseBasedAsyncFunction<T> {
+  return () =>
+    new Promise<T>((resolve, reject) => {
+      fn((response) => {
+        if (response.status === 'success') {
+          resolve(response.data);
+        } else {
+          reject(new Error(response.error));
+        }
+      });
+    });
+}
+
+type SourceObject<T> = { [K in keyof T]: CallbackBasedAsyncFunction<T[K]> };
+type PromisifiedObject<T> = { [K in keyof T]: PromiseBasedAsyncFunction<T[K]> };
+
+export function promisifyAll<T extends { [key: string]: any }>(
+  obj: SourceObject<T>
+): PromisifiedObject<T> {
+  const result: Partial<PromisifiedObject<T>> = {};
+  for (const key of Object.keys(obj) as (keyof T)[]) {
+    result[key] = promisify(obj[key]);
+  }
+  return result as PromisifiedObject<T>;
 }
 
 const oldApi = {
@@ -109,14 +139,16 @@ const oldApi = {
   },
 };
 
-export const api = {
-  requestAdmins: promisify(oldApi.requestAdmins),
-  requestUsers: promisify(oldApi.requestUsers),
-  requestCurrentServerTime: promisify(oldApi.requestCurrentServerTime),
-  requestCoffeeMachineQueueLength: promisify(
-    oldApi.requestCoffeeMachineQueueLength
-  ),
-};
+export const api = promisifyAll(oldApi);
+
+// export const api = {
+//   requestAdmins: promisify(oldApi.requestAdmins),
+//   requestUsers: promisify(oldApi.requestUsers),
+//   requestCurrentServerTime: promisify(oldApi.requestCurrentServerTime),
+//   requestCoffeeMachineQueueLength: promisify(
+//     oldApi.requestCoffeeMachineQueueLength
+//   ),
+// };
 
 function logPerson(person: Person) {
   console.log(
